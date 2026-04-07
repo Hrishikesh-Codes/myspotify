805ab7279c8fbc1c0f2fcb7e1d25d0637eb45aea
Hrishikesh Kini <hrishikeshkini@Hrishikeshs-MacBook-Pro.local>
2026-04-07 13:05:57 -0700

    fix: use sameSite=none in production for cross-origin session cookies

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,7 +38,7 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     },
